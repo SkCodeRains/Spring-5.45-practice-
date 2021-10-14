ddfdddda2a6865ddb8446421fd49e68e53303614
CodeRains <CodeRains@DESKTOP-CE3CK0Q>
2021-10-14 15:07:48 +0530

    upto ioc 3

---
+++ IOCProj3-DependencyInjection-LooselyCoupling/src/main/java/com/rains/test/IOCProj3_DependencyInjection_LooselyCoupling/App.java
@@ -0,0 +1,13 @@
+package com.rains.test.IOCProj3_DependencyInjection_LooselyCoupling;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
